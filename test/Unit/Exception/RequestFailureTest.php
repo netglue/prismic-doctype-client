f42f5007974dac6e9f7611377b71d694c7fcb013
George Steel <george@net-glue.co.uk>
2022-06-06 09:16:04 +0100

    Squash some mutants

--- test/Unit/Exception/RequestFailureTest.php
+++ test/Unit/Exception/RequestFailureTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Prismic\DocumentType\Test\Unit\Exception;
 
+use Laminas\Diactoros\Request;
 use PHPUnit\Framework\TestCase;
 use Prismic\DocumentType\Exception\AssertionFailed;
 use Prismic\DocumentType\Exception\RequestFailure;
+use Psr\Http\Client\ClientExceptionInterface;
 
 class RequestFailureTest extends TestCase
 {
@@ -16,5 +18,26 @@ class RequestFailureTest extends TestCase
         $this->expectExceptionMessage('This error was not constructed with a request instance');
         $error = new RequestFailure('Anything', 0);
         $error->failedRequest();
+    }
+
+    public function testPsrErrorWillHaveTheExpectedCode(): void
+    {
+        $error = RequestFailure::withPsrError(
+            new Request('/foo'),
+            $this->createMock(ClientExceptionInterface::class)
+        );
+
+        self::assertEquals(0, $error->getCode());
+    }
+
+    public function testPsrErrorWillReferencePreviousException(): void
+    {
+        $exception = $this->createMock(ClientExceptionInterface::class);
+        $error = RequestFailure::withPsrError(
+            new Request('/foo'),
+            $exception
+        );
+
+        self::assertSame($exception, $error->getPrevious());
     }
 }
